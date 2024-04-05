63a092d6f858ac828eba6d3a149bf1f8e5aebfa5
gabrielWilfried <gabrielwilfried0808@gmail.com>
2024-04-05 14:01:07 +0200

    SHA-1 password cracker

--- main.py
+++ main.py
@@ -10,5 +10,17 @@ cracked_password2 = password_cracker.crack_sha1_hash(
     "dcc466796201f7232b22a03781110a8871fd038c", True)
 print(cracked_password2)
 
+cracked_password3 = password_cracker.crack_sha1_hash(
+    "b305921a3723cd5d70a375cd21a61e60aabb84ec")
+print(cracked_password3)
+
+cracked_password4 = password_cracker.crack_sha1_hash(
+    "c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")
+print(cracked_password4)
+
+cracked_password5 = password_cracker.crack_sha1_hash(
+    "ea3f62d498e3b98557f9f9cd0d905028b3b019e1", True)
+print(cracked_password5)
+
 # Run unit tests automatically
-main(module = "test_module", exit = False)
+# main(module = "test_module", exit = False)
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,32 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
-    return True
+    #Load the list of top 10000 passwords 
+    with open("top-10000-passwords.txt", "r") as f:
+        passwords = f.readlines()
+    passwords = [password.strip() for password in passwords]
+
+    #Load salt if use_salts is true
+    salts = []
+    if use_salts:
+        with open("known-salts.txt", "r") as f:
+            salts = f.readlines()
+        salts = [salt.strip() for salt in salts]
+
+    # Iterate through each password and salt combination
+    for password in passwords:
+        hashed_password = password.encode()
+        # Apply salts if use_salts is True
+        if use_salts:
+            for salt in salts:
+                # hashed_password = salt.encode() + hashed_password + salt.encode()
+                salted_password = salt.encode() + password.encode() 
+                hashed_password = hashlib.sha1(salted_password).hexdigest()
+        else:
+            # Hash the password
+            hashed_password = hashlib.sha1(hashed_password).hexdigest()
+    
+        # Compare the hashed password with the provided hash
+        if hashed_password == hash:
+            return password
+    return "PASSWORD NOT IN DATABASE"
